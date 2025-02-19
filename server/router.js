afbab144f2a1a3f9112f0b4207e9b458c92bf5d1
danielagmz <161260538+danielagmz@users.noreply.github.com>
2025-02-19 18:23:40 +0100

    feat: rutas

--- server/router.js
+++ server/router.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { IncomingForm } from 'formidable';
+import { AuthController } from './controllers/AuthController.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,11 +16,38 @@ export class RouterConfig {
   }
 
   static configureRoutes () {
+    this.configureGetRoutes();
+    this.configurePostRoutes();
+  }
+
+  static configureGetRoutes () {
     this.router.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/index.html'));
     });
+    this.router.get('/choose', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/html/seleccio_rol.html'));
+    });
+    this.router.get('/player', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/html/player.html'));
+    });
 
-    // Puedes agregar más rutas aquí si es necesario
+    this.router.get('/admin', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/html/admin.html'));
+    });
+  }
+
+  static configurePostRoutes () {
+    this.router.post('/login', (req, res) => {
+      const form = new IncomingForm();
+
+      form.parse(req, (err, fields) => {
+        if (err) {
+          return res.status(400).json({ error: 'Error al procesar el formulario' });
+        }
+
+        AuthController.login(fields, res);
+      });
+    });
   }
 }
 
